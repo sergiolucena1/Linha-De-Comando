937ea88943616435b4f41e7a01d98969d4696af6
Sergio Lucena <slucenadecarvalho@gmail.com>
2024-01-22 11:44:43 -0300

    Criando função responsavel por retornar o IP de um host informado no terminal

--- app/app.go
+++ app/app.go
@@ -1,11 +1,43 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
-//Gerar vai retornar a aplicação de linha de comando, pronta para ser executada
+	"github.com/urfave/cli"
+)
+
+// Gerar vai retornar a aplicação de linha de comando, pronta para ser executada
 func Gerar() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
+
+	app.Commands = []cli.Command{ //comandos e seus valores
+		{
+			Name:  "ip",
+			Usage: "Busca IPS de endereços na Internet",
+			Flags: []cli.Flag{
+				cli.StringFlag{ // flag e seu valor
+					Name:  "host",
+					Value: "amazon.com",
+				},
+			},
+			Action: buscarIps,
+		},
+	}
 	return app
+}
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+	ips, err := net.LookupIP(host) // procura o host passado e retorna o IP
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
