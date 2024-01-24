c568f911ed617ecf98791ff71c75a79475ceb683
Sergio Lucena <slucenadecarvalho@gmail.com>
2024-01-24 18:22:37 -0300

    Função responsável por receber o host e retornar os servidores

--- app/app.go
+++ app/app.go
@@ -14,17 +14,25 @@ func Gerar() *cli.App {
 	app.Name = "Aplicação de linha de comando"
 	app.Usage = "Busca IPs e Nomes de Servidor na internet"
 
+	flags := []cli.Flag{
+		cli.StringFlag{ // flag e seu valor DEFAULT
+			Name:  "host",
+			Value: "amazon.com",
+		},
+	}
+
 	app.Commands = []cli.Command{ //comandos e seus valores
 		{
-			Name:  "ip",
-			Usage: "Busca IPS de endereços na Internet",
-			Flags: []cli.Flag{
-				cli.StringFlag{ // flag e seu valor
-					Name:  "host",
-					Value: "amazon.com",
-				},
-			},
+			Name:   "ip",
+			Usage:  "Busca IPS de endereços na Internet",
+			Flags:  flags,
 			Action: buscarIps,
+		},
+		{
+			Name:   "servidores",
+			Usage:  "Busca o nome dos servidores na internet",
+			Flags:  flags,
+			Action: buscarServidores,
 		},
 	}
 	return app
@@ -39,5 +47,17 @@ func buscarIps(c *cli.Context) {
 
 	for _, ip := range ips {
 		fmt.Println(ip)
+	}
+}
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+	servidores, err := net.LookupNS(host) // procura o host passado e retorna o nome do servidor
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, servidor := range servidores {
+		fmt.Println(servidor.Host)
 	}
 }
